b0aa407cb61690656b6be88985707b1b4d9efc1d
yoongkaroompa <a5312559@gmail.com>
2023-03-05 07:14:15 +0900

    add files

---
+++ mbti-react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MbtiRouter from "./Router";
+
+function App() {
+  return (
+    <div className="App">
+      <MbtiRouter />
+    </div>
+  );
+}
+
+export default App;
